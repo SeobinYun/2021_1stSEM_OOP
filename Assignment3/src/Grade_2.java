684251ca6c6e5eedd2d1a83c82b1f4cd9a5488c0
SeobinYun <o0oseobin@naver.com>
2021-05-18 01:07:02 +0900

    edit Grade_2

--- Assignment3/src/Grade_2.java
+++ Assignment3/src/Grade_2.java
@@ -12,7 +12,12 @@ public class Grade_2 {
     }
 
     private int average(){
-        return (math+science+english)/3;
+        if(math>=0 && science>=0 && english>=0) {
+            return (math + science + english) / 3;
+        }
+        else{
+            throw new IllegalArgumentException("입력된 수가 0 이상이 아닙니다.");
+        }
     }
 
     public static void main(String[] args){
