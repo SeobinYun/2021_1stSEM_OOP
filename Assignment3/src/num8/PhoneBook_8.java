43b77270e4d50104b8ddd7f147d6c95ecf2342d8
SeobinYun <o0oseobin@naver.com>
2021-05-18 22:07:04 +0900

    add comment & edit search part

--- Assignment3/src/num8/PhoneBook_8.java
+++ Assignment3/src/num8/PhoneBook_8.java
@@ -1,35 +1,36 @@
 package num8;
 
-import java.util.Scanner;
+import java.util.Scanner; // Scanner 사용
 
 public class PhoneBook_8 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.print("인원수>>");
-        int peopleCount = scanner.nextInt();
-        Phone[] infoArray = new Phone[peopleCount];
-        for(int i=0; i<peopleCount; i++){
+        int peopleCount = scanner.nextInt(); // 인원수 입력받음
+
+        Phone[] infoArray = new Phone[peopleCount]; // 입력받은 인원수만큼 Phone[] 배열 객체 생성
+        for(int i=0; i<peopleCount; i++){ // 입력받은 인원수만큼 이름, 연락처 입력받기
             System.out.print("이름과 전화번호(이름과 번호는 빈 칸없이 입력)>>");
             String nameInput = scanner.next();
             String telInput = scanner.next();
-            infoArray[i] = new Phone(nameInput, telInput);
+            infoArray[i] = new Phone(nameInput, telInput); // 생성자로 넘겨줌
         }
         System.out.println("저장되었습니다...");
 
         while(true){
             System.out.print("검색할 이름>>");
             String findInfo = scanner.next();
-            if(findInfo.equals("그만")){
+            if(findInfo.equals("그만")){ // 입력된 findInfo가 "그만"이라는 문자열이면 그만함
                 break;
             }
             else{
-                for(int i=0; i<infoArray.length; i++){
-                    if(infoArray[i].getName().equals(findInfo)){
+                for(int i=0; i<infoArray.length; i++){ // infoArray 배열의 길이만큼 for문을 돌려 정보가 있는지 탐색
+                    if(infoArray[i].getName().equals(findInfo)){ // 있으면 sout으로 출력
                         System.out.println(infoArray[i].getName() + "의 번호는 "+infoArray[i].getTel()+" 입니다.");
                         break;
                     }
-                    else if(i==infoArray.length-1){
-                        System.out.println(infoArray[i].getName() + "이 없습니다.");
+                    else if(i==infoArray.length-1){ // 마지막까지 탐색했는데 없으면 없다고 출력
+                        System.out.println(findInfo + "이 없습니다.");
                     }
                 }
             }
