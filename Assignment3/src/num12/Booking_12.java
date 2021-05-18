f8c97d1a1d3fd65cdcfc7c6f874bad7c4896a3a9
SeobinYun <o0oseobin@naver.com>
2021-05-19 00:31:36 +0900

    complete Booking_12 on my_space

--- Assignment3/src/num12/Booking_12.java
+++ Assignment3/src/num12/Booking_12.java
@@ -1,188 +1,189 @@
 package num12;
 
 import java.sql.SQLOutput;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-class Seat{
-    String[] S;
-    String[] A;
-    String[] B;
-    public Seat() {
-        //String[] S = new String[10];
-        //String[] A = new String[10];
-        //String[] B = new String[10];
-        String[] S = {"---", "---", "---", "---", "---", "---", "---", "---", "---", "---"};
-        String[] A = {"---", "---", "---", "---", "---", "---", "---", "---", "---", "---"};
-        String[] B = {"---", "---", "---", "---", "---", "---", "---", "---", "---", "---"};
-
-    }
-}
 
 public class Booking_12 {
     public static void main(String[] args){
-        Seat ex = new Seat();
+        String[] S = {"---","---","---","---","---","---","---","---","---","---"};
+        String[] A = {"---","---","---","---","---","---","---","---","---","---"};
+        String[] B = {"---","---","---","---","---","---","---","---","---","---"};
+        String[][] array = {S,A,B};
 
-
-        for(int i=0; i<ex.S.length; i++){
-            System.out.print(" " + ex.S[i]);
-        }
         Scanner scanner = new Scanner(System.in);
         System.out.println("명품콘서트홀 예약 시스템입니다.");
         while(true) {
             System.out.print("예약:1, 조회:2, 취소:3, 끝내기:4>>");
-            int num = scanner.nextInt();
-            switch (num) {
-                case 1:
-                    reservation(ex);
-                    break;
-                case 2:
-                    check(ex);
-                    break;
-                case 3:
-                    cancel(ex);
-                    break;
-                case 4:
-                    return;
-                default:
-                    System.out.println("1~4 이외의 수를 입력하셨습니다. 다시 실행해주세요.");
+            try {
+                int num = scanner.nextInt();
+                switch (num) {
+                    case 1:
+                        reservation(array);
+                        break;
+                    case 2:
+                        check(array);
+                        break;
+                    case 3:
+                        cancel(array);
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        System.out.println("1~4 이외의 수를 입력하셨습니다. 다시 실행해주세요.");
+                }
+            }
+            catch (InputMismatchException e){
+                System.out.println("잘못된 형식을 입력하셨습니다.");
             }
         }
     }
 
-    static void reservation(Seat a){
+    static void reservation(String[][] a){
         Scanner scanner = new Scanner(System.in);
         System.out.print("좌석구분 S(1), A(2), B(3)>>");
-        int num = scanner.nextInt();
-        String name;
-        int where;
-        switch (num){
-            case 1:
-                System.out.print("S>>");
-                for(int i=0; i<a.S.length; i++){
-                    System.out.print(" " + a.S[i]);
-                }
-                System.out.print("\n이름>>");
-                name = scanner.next();
-                do {
-                    System.out.print("번호>>");
-                    where = scanner.nextInt();
-                }while(where>10 || where<1);
+        try {
+            int num = scanner.nextInt();
+            String name;
+            int where;
+            switch (num) {
+                case 1:
+                    System.out.print("S>>");
+                    for (int i = 0; i < a[0].length; i++) {
+                        System.out.print(" " + a[0][i]);
+                    }
+                    System.out.print("\n이름>>");
+                    name = scanner.next();
+                    do {
+                        System.out.print("번호>>");
+                        where = scanner.nextInt();
+                    } while (where > 10 || where < 1);
 
-                a.S[where] = name;
-                break;
-            case 2:
-                System.out.print("A>>");
-                for(int i=0; i<a.A.length; i++){
-                    System.out.print(" " + a.A[i]);
-                }
-                System.out.print("\n이름>>");
-                name = scanner.next();
-                do {
-                    System.out.print("번호>>");
-                    where = scanner.nextInt();
-                }while(where>10 || where<1);
-                a.A[where] = name;
-                break;
-            case 3:
-                System.out.print("B>>");
-                for(int i=0; i<a.B.length; i++){
-                    System.out.print(" " + a.B[i]);
-                }
-                System.out.print("\n이름>>");
-                name = scanner.next();
-                do {
-                    System.out.print("번호>>");
-                    where = scanner.nextInt();
-                }while(where>10||where<1);
-                a.B[where] = name;
-                break;
-            default:
-                System.out.println("1~3 이외의 수를 입력하셨습니다. 다시 실행해주세요.");
-                return;
+                    a[0][where-1] = name;
+                    break;
+                case 2:
+                    System.out.print("A>>");
+                    for (int i = 0; i < a[1].length; i++) {
+                        System.out.print(" " + a[1][i]);
+                    }
+                    System.out.print("\n이름>>");
+                    name = scanner.next();
+                    do {
+                        System.out.print("번호>>");
+                        where = scanner.nextInt();
+                    } while (where > 10 || where < 1);
+                    a[1][where-1] = name;
+                    break;
+                case 3:
+                    System.out.print("B>>");
+                    for (int i = 0; i < a[2].length; i++) {
+                        System.out.print(" " + a[2][i]);
+                    }
+                    System.out.print("\n이름>>");
+                    name = scanner.next();
+                    do {
+                        System.out.print("번호>>");
+                        where = scanner.nextInt();
+                    } while (where > 10 || where < 1);
+                    a[2][where-1] = name;
+                    break;
+                default:
+                    System.out.println("1~3 이외의 수를 입력하셨습니다. 다시 실행해주세요.");
+                    return;
+            }
+        }
+        catch(InputMismatchException e){
+            System.out.println("잘못된 형식을 입력하셨습니다.");
         }
     }
 
-    static void check(Seat a){
+    static void check(String[][] a){
         System.out.print("S>>");
-        for(int i=0; i<a.S.length; i++){
-            System.out.print(" " + a.S[i]);
+        for(int i=0; i<a[0].length; i++){
+            System.out.print(" " + a[0][i]);
         }
         System.out.println();
 
         System.out.print("A>>");
-        for(int i=0; i<a.A.length; i++){
-            System.out.print(" " + a.A[i]);
+        for(int i=0; i<a[1].length; i++){
+            System.out.print(" " + a[1][i]);
         }
         System.out.println();
 
         System.out.print("B>>");
-        for(int i=0; i<a.B.length; i++){
-            System.out.print(" " + a.B[i]);
+        for(int i=0; i<a[2].length; i++){
+            System.out.print(" " + a[2][i]);
         }
         System.out.println();
         System.out.println("<<<조회를 완료하였습니다.>>>");
     }
 
-    static void cancel(Seat a){
+    static void cancel(String[][] a){
         Scanner scanner = new Scanner(System.in);
         System.out.print("좌석 S:1, A:2, B:3>>");
-        int num = scanner.nextInt();
-        String name;
-        switch (num){
-            case 1:
-                System.out.print("S>>");
-                for(int i=0; i<a.S.length; i++){
-                    System.out.print(" " + a.S[i]);
-                }
-                System.out.print("\n이름>>");
-                name = scanner.next();
-                for(int i=0; i<a.S.length; i++){
-                    if (a.S[i].equals(name)) {
-                        a.S[i] = "---";
-                        return;
+        try{
+            int num = scanner.nextInt();
+            String name;
+            switch (num){
+                case 1:
+                    System.out.print("S>>");
+                    for(int i=0; i<a[0].length; i++){
+                        System.out.print(" " + a[0][i]);
                     }
-                    else if(i==a.S.length-1){
-                        System.out.println(name+"은 예약자 목록에 없습니다.");
+                    System.out.print("\n이름>>");
+                    name = scanner.next();
+                    for(int i=0; i<a[0].length; i++){
+                        if (a[0][i].equals(name)) {
+                            a[0][i] = "---";
+                            return;
+                        }
+                        else if(i==a[0].length-1){
+                            System.out.println(name+"은 예약자 목록에 없습니다.");
+                        }
                     }
-                }
-                break;
-            case 2:
-                System.out.print("A>>");
-                for(int i=0; i<a.A.length; i++){
-                    System.out.print(" " + a.A[i]);
-                }
-                System.out.print("\n이름>>");
-                name = scanner.next();
-                for(int i=0; i<a.A.length; i++){
-                    if (a.A[i].equals(name)) {
-                        a.A[i] = "---";
-                        return;
+                    break;
+                case 2:
+                    System.out.print("A>>");
+                    for(int i=0; i<a[1].length; i++){
+                        System.out.print(" " + a[1][i]);
                     }
-                    else if(i==a.A.length-1){
-                        System.out.println(name+"은 예약자 목록에 없습니다.");
+                    System.out.print("\n이름>>");
+                    name = scanner.next();
+                    for(int i=0; i<a[1].length; i++){
+                        if (a[1][i].equals(name)) {
+                            a[1][i] = "---";
+                            return;
+                        }
+                        else if(i==a[1].length-1){
+                            System.out.println(name+"은 예약자 목록에 없습니다.");
+                        }
                     }
-                }
-                break;
-            case 3:
-                System.out.print("B>>");
-                for(int i=0; i<a.B.length; i++){
-                    System.out.print(" " + a.B[i]);
-                }
-                System.out.print("\n이름>>");
-                name = scanner.next();
-                for(int i=0; i<a.B.length; i++){
-                    if (a.B[i].equals(name)) {
-                        a.B[i] = "---";
-                        return;
+                    break;
+                case 3:
+                    System.out.print("B>>");
+                    for(int i=0; i<a[2].length; i++){
+                        System.out.print(" " + a[2][i]);
                     }
-                    else if(i==a.B.length-1){
-                        System.out.println(name+"은 예약자 목록에 없습니다.");
+                    System.out.print("\n이름>>");
+                    name = scanner.next();
+                    for(int i=0; i<a[2].length; i++){
+                        if (a[2][i].equals(name)) {
+                            a[2][i] = "---";
+                            return;
+                        }
+                        else if(i==a[2].length-1){
+                            System.out.println(name+"은 예약자 목록에 없습니다.");
+                        }
                     }
-                }
-                break;
-            default:
-                System.out.println("1~3 이외의 수를 입력하셨습니다. 다시 실행해주세요.");
-                return;
+                    break;
+                default:
+                    System.out.println("1~3 이외의 수를 입력하셨습니다. 다시 실행해주세요.");
+                    return;
+            }
+        }
+        catch(InputMismatchException e){
+            System.out.println("잘못된 형식을 입력하셨습니다.");
         }
     }
 
